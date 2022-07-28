bfea771bc3108dca0e0c18cf4066cb8491f51341
Anwar <007anwarmulla@gmail.com>
2022-07-28 17:47:52 +0530

    added partitions

--- src/main/java/com/example/orderproducer/api/ProducerController.java
+++ src/main/java/com/example/orderproducer/api/ProducerController.java
@@ -1,9 +1,12 @@
 package com.example.orderproducer.api;
 
+import com.example.orderproducer.customserializer.UserSerializer;
 import com.example.orderproducer.dto.UserDetails;
+import com.example.orderproducer.service.Constants;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerRecord;
 import org.apache.kafka.clients.producer.RecordMetadata;
+import org.apache.kafka.common.serialization.StringSerializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -24,10 +27,10 @@ public class ProducerController {
         Logger log = LoggerFactory.getLogger(ProducerController.class);
         Properties properties = new Properties();
         properties.setProperty("bootstrap.servers","localhost:9092");
-        properties.setProperty("key.serializer","org.apache.kafka.common.serialization.StringSerializer");
-        properties.setProperty("value.serializer","com.example.orderproducer.customserializer.UserSerializer");
+        properties.setProperty("key.serializer", StringSerializer.class.getName());
+        properties.setProperty("value.serializer", UserSerializer.class.getName());
         KafkaProducer<Integer, UserDetails> producer = new KafkaProducer<>(properties);
-        ProducerRecord<Integer, UserDetails> apimessages = new ProducerRecord<>("userdetails", userDetails);
+        ProducerRecord<Integer, UserDetails> apimessages = new ProducerRecord<>(Constants.PARTITIONED, userDetails);
         log.info("Sending userDetails to kafka");
         Future<RecordMetadata> send = producer.send(apimessages);
         log.info("Message send to kafka topic: {}",send.get().topic());
